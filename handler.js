0e9463a229d48d2513db0e7c496ec390b0ceacfe
Apoorv Vardhan <vardhanapoorv@gmail.com>
2018-12-14 07:05:31 +0530

    serverless github pr check

---
+++ handler.js
@@ -0,0 +1,36 @@
+'use strict';
+const Github = require("github-client")
+
+var Closing_keywords = [
+  "close",
+  "closes",
+  "closed",
+  "fix",
+  "fixes",
+  "fixed",
+  "resolve",
+  "resolves",
+  "resolved"
+]
+
+module.exports.githubPRCheck = (event, context, callback) => {
+  var gh = Github.new({
+    token: "8751df2d6d1dc6e82e36a5bcb28684baf1447445"
+  });
+  const body = JSON.parse(event.body);
+  const { pull_request, repository } = body;
+  const repository_name = repository.full_name;
+  const { sha } = pull_request.head;
+  const pr_body = pull_request.body;
+    for (var words of Closing_keywords) {
+      if(pr_body.indexOf(words) > -1){
+        console.log("well done")
+        return true; 
+      }
+    }
+     console.log("Pull request hasn't fixed anything Fuck off!!!")
+     return false
+        
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
